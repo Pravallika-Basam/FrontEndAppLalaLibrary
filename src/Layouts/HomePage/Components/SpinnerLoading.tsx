597856f5301653877eab0e219782068799969657
Pravallika-Basam <pravalikachowdary11@gmail.com>
2023-06-02 13:32:43 -0400

    first save

---
+++ src/Layouts/HomePage/Components/SpinnerLoading.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SpinnerLoading = () => {
+  return (
+  <div className="spinner-grow text-info" role="status">
+  <span className="sr-only">Loading...</span>
+  </div>
+    );
+}
